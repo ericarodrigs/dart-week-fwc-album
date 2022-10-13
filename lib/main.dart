fb80debec7620e828d69a2a9157beca6b3896e78
Erica Rodrigues <ericamrodrigs@gmail.com>
2022-10-13 00:22:09 -0300

    Configures dot env

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:dart_week_worldcup_album/app/core/config/env/env.dart';
 import 'package:dart_week_worldcup_album/app/fwc_album_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  await Env.i.load();
   runApp(const FwcAlbumApp());
 }
