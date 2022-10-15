675fb0db42c81634344d186deb5dcc50ffbcb832
Erica Rodrigues <ericamrodrigs@gmail.com>
2022-10-15 12:42:18 -0300

    Creates home page and extract some components

--- lib/app/core/ui/styles/text_styles.dart
+++ lib/app/core/ui/styles/text_styles.dart
@@ -64,6 +64,8 @@ class TextStyles {
   TextStyle get titleWhite => textPrimaryFontBold.copyWith(color: Colors.white, fontSize: 22);
 
   TextStyle get titleBlack => textPrimaryFontBold.copyWith(color: Colors.black, fontSize: 22);
+
+  TextStyle get titlePrimaryColor => textPrimaryFontBold.copyWith(color: ColorsApp.i.primary, fontSize: 22);
 }
 
 extension TextStylesExtensions  on BuildContext {
--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,7 +1,10 @@
-import 'package:dart_week_worldcup_album/app/core/rest/custom_dio.dart';
+import 'package:dart_week_worldcup_album/app/core/ui/styles/button_styles.dart';
+import 'package:dart_week_worldcup_album/app/core/ui/styles/colors_app.dart';
+import 'package:dart_week_worldcup_album/app/core/ui/styles/text_styles.dart';
+import 'package:dart_week_worldcup_album/app/core/ui/widgets/button.dart';
+import 'package:dart_week_worldcup_album/app/pages/home/widgets/status_tile.dart';
+import 'package:dart_week_worldcup_album/app/pages/home/widgets/sticker_percent_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_getit/flutter_getit.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,15 +12,91 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: context.colors.primary,
       appBar: AppBar(
-        title: const Text('Home Page'),
+        elevation: 0,
+        backgroundColor: context.colors.primary,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.logout,
+                color: Colors.white,
+              ))
+        ],
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            context.get<CustomDio>().auth().get('/api/me');
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: LayoutBuilder(
+          builder: (_, constrains) {
+            return ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constrains.maxHeight),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.only(bottom: 25),
+                        width: MediaQuery.of(context).size.width,
+                        child: Image.asset(
+                          'assets/images/bola.png',
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const StickerPercentWidget(
+                        percent: 60,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        '45 figurinhas',
+                        style: context.textStyles.titleWhite,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      StatusTile(
+                          icon: Image.asset('assets/images/all_icon.png'),
+                          label: 'Todas',
+                          value: 45),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      StatusTile(
+                          icon: Image.asset('assets/images/missing_icon.png'),
+                          label: 'Faltando',
+                          value: 500),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      StatusTile(
+                          icon: Image.asset('assets/images/repeated_icon.png'),
+                          label: 'Repetidas',
+                          value: 30),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Button(
+                          onPressed: () {},
+                          width: MediaQuery.of(context).size.width * .9,
+                          outlined: true,
+                          style: context.buttonStyles.yellowOutlinedButton,
+                          labelStyle: context
+                              .textStyles.textSecondaryFontExtraBold
+                              .copyWith(color: context.colors.yellow),
+                          label: 'Minhas Figurinhas'),
+                    ],
+                  ),
+                ),
+              ),
+            );
           },
-          child: const Text('Testando auth'),
         ),
       ),
     );
